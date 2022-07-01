a1886cf6cf135f348cd2e24300e35261b36c41f7
Jaycee Porto <jaycee.porto@outlook.com>
2022-07-01 17:34:03 -0500

    Changes Made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Component } from "react";
+import Box from "./components/Box";
 
 class App extends Component {
   constructor(props) {
@@ -17,8 +18,27 @@ class App extends Component {
 
     // set default state
 
+    this.state = {
+      boxes: BOXES,
+    }
+    
     // bind methods to this
+    this.handleClick = this.handleClick.bind(this);
   }
+
+    getRandomColor() {
+      return Math.round(Math.random() * 255);
+    }
+
+    handleClick = (event) => {
+      let updatedBoxes = this.state.boxes.map((box) => {
+        if (box.id == event.target.id) {
+          box.color = `rgb(${this.getRandomColor()}, ${this.getRandomColor()}, ${this.getRandomColor()})`
+        }
+        return box;
+      });
+      this.setState({boxes: updatedBoxes})
+    };
 
   render() {
     return (
@@ -31,7 +51,14 @@ class App extends Component {
         }}
       >
         <h1>React: State and Props</h1>
-        <div className="App">{/* render boxes */}</div>
+        <div className="App">{this.state.boxes.map((box) => {
+          return <Box 
+          key ={box.id} 
+          id={box.id} 
+          color={box.color} 
+          onClick={this.handleClick}/>
+        })}
+        </div>
       </main>
     );
   }
